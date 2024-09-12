53f458dc89f76d18c40df1258e0f962334444c90
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-11 21:20:53 -0400

    Day01

---
+++ tests/day01/playwrightIntro.spec.js
@@ -0,0 +1,23 @@
+import {test} from '@playwright/test';
+
+test("Simple playwright test", async ({page}) => { 
+    await page.goto("https://www.google.com/");
+
+    await page.waitForTimeout(3000);
+
+    let searchBox = page.locator("//textarea[@class='gLFyf']");
+
+    await searchBox.fill("Playwright Automation");
+
+    //await searchBox.type("Playwright Automation");
+
+    await page.waitForTimeout(3000);
+
+ }); 
+
+/*
+
+<textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="false" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwid2NOYlryIAxUxD1kFHVEaHyUQ39UDCAY"></textarea>
+*/
+
+//textarea[@class='gLFyf']
