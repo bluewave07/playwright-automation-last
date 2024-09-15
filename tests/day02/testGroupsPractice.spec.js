b665ac06e924532af6b2d9d31b3527dae84d702e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-15 13:31:49 -0400

    Day02

--- tests/day02/testGroupsPractice.spec.js
+++ tests/day02/testGroupsPractice.spec.js
@@ -1,3 +1,26 @@
 
+import { test } from '@playwright/test';
 
-// come back at 1:15 pm
+test.describe('Test Group1', () => {
+
+   // create beforeEach for tests
+   test.beforeEach(async ({ page }) => { 
+        await page.goto("https://practice.cydeo.com/");
+   });
+
+   // create afterEach for tests
+    test.afterEach(async ({ page }) => { 
+        await page.waitForTimeout(3000);
+   });
+
+    
+  test('Getting the title of the page', async ({ page }) => {
+    console.log(await page.title());
+  });
+
+  test('Getting the current URL of the page', async ({ page }) => {
+    console.log(page.url());
+  });
+
+});
+
