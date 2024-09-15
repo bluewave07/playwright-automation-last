be6a4ca3d11ff0b09a32ec1e2ede0766dd59f9e0
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-15 11:58:09 -0400

    Day02

--- tests/day02/playwrightPractice.spec.js
+++ tests/day02/playwrightPractice.spec.js
@@ -6,14 +6,24 @@ test('Youtube Search', async ({ page }) => {
 
    await page.waitForTimeout(3000);
 
-   page.locator("//input[@name=\"search_query\" and @id =\"search\"]");
+   let searchBox = page.locator("//input[@id='search']");
 
+   await searchBox.click();
 
+   await searchBox.fill('Cydeo');
+
+   await page.waitForTimeout(3000);
+
+   await searchBox.press("Enter");
+
+   await page.waitForTimeout(3000);
+
+   let firstResult = page.locator("(//a[@id='video-title'])[1]");
+
+   await firstResult.click();
+
+   // await page.waitForTimeout(10000);
 
 });
 
-/*
-<input id="search" autocapitalize="none" autocomplete="off" autocorrect="off" name="search_query" tabindex="0" type="text" spellcheck="false" placeholder="Search" aria-label="Search" role="combobox" aria-haspopup="false" aria-autocomplete="list" dir="ltr" class="ytd-searchbox" style="outline: none;">
-*/
 
-//input[@name='search_query' and @id ='search']
