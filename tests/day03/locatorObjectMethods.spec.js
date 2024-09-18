f10a264875e84804f76e60cf88cf1d5ec36ed3c1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-18 18:56:05 -0400

    Day03

--- tests/day03/locatorObjectMethods.spec.js
+++ tests/day03/locatorObjectMethods.spec.js
@@ -1,30 +1,41 @@
-import { test } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Test Group', () => {
-
-    // create a beforeEach
-    test.beforeEach(async ({ page }) => { 
-        await page.goto("https://practice.cydeo.com/");
-    });
-
-
-  test('getAttribute() retrives the attribute value', async ({ page }) => {
-
-    console.log(await page.locator("text='A/B Testing'").getAttribute("href")); 
-
-    console.log( await page.locator("text='Home'").getAttribute("class") );
-
+test.describe("Test Group", () => {
+  // create a beforeEach
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practice.cydeo.com/");
   });
 
+  test("getAttribute() retrives the attribute value", async ({ page }) => {
+    console.log(await page.locator("text='A/B Testing'").getAttribute("href"));
 
-  test('B', async ({ page }) => {
-   
+    console.log(await page.locator("text='Home'").getAttribute("class"));
   });
 
-  test('C', async ({ page }) => {
-    // Test implementation goes here
-  });
+  test("state methods of locator", async ({ page }) => {
+    let testAutomationPractice = page.locator("//span[@class='h1y']");
 
+    console.log(await testAutomationPractice.isVisible());
+
+    expect(await testAutomationPractice.isVisible()).toBeTruthy(); // verifies that the element is visible
+
+    await expect(testAutomationPractice).toBeVisible(); // passing webElement to the expect
+
+    console.log("--------------------------------");
+
+    let autoComplete = page.locator("text='Autocomplete'");
+
+    console.log(await autoComplete.isEnabled());
+
+    expect(await autoComplete.isEnabled()).toBeTruthy(); // verifies that the element is enabled
+
+    await expect(autoComplete).toBeEnabled(); // verifies that the element is enabled
+
+    console.log("--------------------------------");
+
+
+  });
+  
 });
 /*
 <a class="nav-link" href="/">Home</a>
