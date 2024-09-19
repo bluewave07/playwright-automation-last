b5b9abb0c1c1e96ca630c89d219ab8b29eba40ce
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-18 20:25:24 -0400

    Day03

--- tests/day03/sepLogin.spec.js
+++ tests/day03/sepLogin.spec.js
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test';
 
 test('SEP Login', async ({ page }) => {
 
-    const code = Buffer.from("automation-user:123abc").toString("base64");
+    const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
 
     await page.setExtraHTTPHeaders({
         Authorization: `Basic ${code}`
     });
 
-    await page.goto("https://qa.sep.tdtm.cydeo.com/taws");
+    await page.goto(process.env.SEP_URL);
 
     await page.waitForTimeout(5000);
 
