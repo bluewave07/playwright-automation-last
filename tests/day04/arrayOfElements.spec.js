631ef9c8cecf3701edd6fc645f6cc7a3eb511bf5
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-22 11:23:27 -0400

    Day04

--- tests/day04/arrayOfElements.spec.js
+++ tests/day04/arrayOfElements.spec.js
@@ -1,49 +1,48 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test.describe('Test Group', () => {
+test.describe("Test Group", () => {
 
-  test('Verify that there are exactly 50 link elements within the <ul> element', async ({ page }) => {
+    let elements;
+
+  // create a beforeEach function
+  test.beforeEach(async ({ page }) => {
     await page.goto("https://practice.cydeo.com/");
-    let elements = await page.locator("//ul[@class='list-group']/li").all();
-    expect(elements.length).toBe(50);
+    elements = await page.locator("//ul[@class='list-group']/li/a").all();
+    
+  });
 
+  test("Verify that there are exactly 50 link elements within the <ul> element", async ({page,}) => {
+    expect(elements.length).toBe(50);
     // another way:
     expect(await page.locator("//ul[@class='list-group']/li").count()).toBe(50);
   });
 
-
-  test('Verify that each of the 50 link elements within the <ul> element is visible', async ({ page }) => {
-    await page.goto("https://practice.cydeo.com/");
-    let elements = await page.locator("//ul[@class='list-group']/li/a").all();
-
-    for(let element of elements){
-       await expect(element).toBeVisible();
+  test("Verify that each of the 50 link elements within the <ul> element is visible", async ({page,}) => {
+    for (let element of elements) {
+      await expect(element).toBeVisible();
       // expect( await element.isVisible()).toBeTruthy();
     }
-
   });
 
-
-  test('Verify that each of the 50 link elements within the <ul> element is enabled', async ({ page }) => {
-    await page.goto("https://practice.cydeo.com/");
-    let elements = await page.locator("//ul[@class='list-group']/li/a").all();
-
-    for(let i = 0; i < elements.length; i++){
-        await expect(elements[i]).toBeEnabled();
-       // expect(await elements[i].isEnabled()).toBeTruthy();
+  test("Verify that each of the 50 link elements within the <ul> element is enabled", async ({page,}) => {
+    for (let i = 0; i < elements.length; i++) {
+      await expect(elements[i]).toBeEnabled();
+      // expect(await elements[i].isEnabled()).toBeTruthy();
     }
-
   });
 
-  test('Verify that each of the 50 link elements within the <ul> element has a valid `href` attribute', async ({ page }) => {
-    await page.goto("https://practice.cydeo.com/");
-    let elements = await page.locator("//ul[@class='list-group']/li/a").all();
+  test("Verify that each of the 50 link elements within the <ul> element has a valid `href` attribute", async ({page,}) => {
+    /*
+    for (let element of elements) {
+      let href = await element.getAttribute("href");
+      expect(href).toBeTruthy();
+    }*/
 
-    for(let element of elements){
-        let href = await element.getAttribute("href");
-        expect(href).toBeTruthy();
-    }
-         
+    elements.forEach(async (element) => {
+      let href = await element.getAttribute("href");
+      expect(href).toBeTruthy();
+      console.log(href);
+    });
 
   });
 
