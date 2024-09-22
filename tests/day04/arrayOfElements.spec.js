954eace591d45752dcf4e5f1724b90d27cc2a0dd
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-22 11:00:04 -0400

    Day04

--- tests/day04/arrayOfElements.spec.js
+++ tests/day04/arrayOfElements.spec.js
@@ -10,14 +10,29 @@ test.describe('Test Group', () => {
     // another way:
     expect(await page.locator("//ul[@class='list-group']/li").count()).toBe(50);
   });
-  
+
 
   test('Verify that each of the 50 link elements within the <ul> element is visible', async ({ page }) => {
-    
+    await page.goto("https://practice.cydeo.com/");
+    let elements = await page.locator("//ul[@class='list-group']/li").all();
+
+    for(let element of elements){
+       await expect(element).toBeVisible();
+       expect( await element.isVisible()).toBeTruthy();
+    }
+
   });
 
+
   test('Verify that each of the 50 link elements within the <ul> element is enabled', async ({ page }) => {
-   
+    await page.goto("https://practice.cydeo.com/");
+    let elements = await page.locator("//ul[@class='list-group']/li").all();
+
+    for(let i = 0; i < elements.length; i++){
+        await expect(elements[i]).toBeEnabled();
+       // expect(await elements[i].isEnabled()).toBeTruthy();
+    }
+
   });
 
   test('Verify that each of the 50 link elements within the <ul> element have a valid `href` attribute', async ({ page }) => {
