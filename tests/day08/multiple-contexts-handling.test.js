788a644901a52d9ca13375b7e0300ef71218ad80
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-10-06 12:34:07 -0400

    Day08

--- tests/day08/multiple-contexts-handling.test.js
+++ tests/day08/multiple-contexts-handling.test.js
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, chromium, firefox } from '@playwright/test';
 
 test('Creating multiple contexts', async ({ browser }) => {
 
@@ -27,7 +27,26 @@ test('Creating multiple contexts', async ({ browser }) => {
 
     await page4.waitForTimeout(3000);
 
+});
 
 
-  
+
+test('', async () => {
+ 
+    let chromeBrowser = await chromium.launch();
+    let chromeContext = await chromeBrowser.newContext();
+    let page1 = await chromeContext.newPage();
+    await page1.goto("https://www.google.com/");
+    let page2 = await chromeContext.newPage();
+    await page2.goto("https://www.youtube.com/");
+
+    await page2.waitForTimeout(3000);
+
+    let firefoxBrowser = await firefox.launch();
+    let firefoxContext = await firefoxBrowser.newContext();
+    let page3 = await firefoxContext.newPage();
+    await page3.goto("https://cydeo.com/");
+
+    await page3.waitForTimeout(3000);
+
 });
